43aea1543c947d3e243c59cf4128d9f0d34caf84
jmwalker49 <jmwalker49@gmail.com>
2019-07-26 15:53:25 -0400

    auth state change listener

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,13 @@
+// listen for auth status change
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('user logged in: ', user);
+    } else{
+        console.log('user logged out');
+    }
+});
+
+
 // sign up
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -21,7 +31,26 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault;
-    auth.signOut().then(() => {
-        console.log("user has logged out");
-    });
+    auth.signOut()
 });
+
+// login method
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault;
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+        
+
+    })
+
+})
